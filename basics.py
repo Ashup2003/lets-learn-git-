e380f0d9281fdf7eed6173e32f03067153d805f2
Ashutosh <ashutosh14003@gmail.com>
2025-12-01 23:19:10 +0530

    initial commit

---
+++ basics.py
@@ -0,0 +1,29 @@
+# yo this is comments
+print("line1")
+print("line2")
+print("line3")
+
+#datatypes
+print('words') #Strings 
+print("words")
+print(123) #integers/ints
+print(-10)
+print(1.5) #float
+
+#operations
+print(3+3)
+print(3-3)
+print(3*3)
+print(3/3)
+
+#variables
+test = 123
+test += 50
+print(test)
+
+#input
+user_input = input("Please write something ")
+print(user_input)
+
+greater_app = input("Enter your name ")
+print('Hello',greater_app,'Have a nice day')
